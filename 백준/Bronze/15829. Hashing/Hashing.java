68f8cd48f6c967b3677e3abe6def1fcf942e7b26
YeongJoon <60257302+Yjooon@users.noreply.github.com>
2025-01-20 14:40:51 +0900

    [Bronze II] Title: Hashing, Time: 68 ms, Memory: 11508 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,21 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	final static int base = 31;
+	final static int mod = 1234567891;
+
+	public static long func(int base, int power) {
+		long result = 1;
+		for (int i = 0; i < power; i++) {
+			result = (result * base) % mod;
+		}
+		return result;
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int L = Integer.parseInt(br.readLine());
 
 		String s = br.readLine();
 		long answer = 0;
-		int base = 31;
-		int mod = 1234567891;
-		int result = 1;
+
 		for (int i = 0; i < L; i++) {
 			char ch = s.charAt(i);
 
-			answer += (ch - 'a' + 1) * (Math.pow(base, i) % mod);
+			answer += ((ch - 'a' + 1) * (func(base, i) % mod)) % mod;
+
 		}
 		answer %= mod;
 		System.out.println(answer);
 	}
+
 }
