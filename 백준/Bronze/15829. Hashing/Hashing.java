7d163b2f73f595fb678461d2795ea6adf33ba8be
YeongJoon <60257302+Yjooon@users.noreply.github.com>
2025-01-20 14:02:46 +0900

    [Bronze II] Title: Hashing, Time: 68 ms, Memory: 11492 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int L = Integer.parseInt(br.readLine());
+
+		String s = br.readLine();
+		long answer = 0;
+		int base = 31;
+		int mod = 1234567891;
+		int result = 1;
+		for (int i = 0; i < L; i++) {
+			char ch = s.charAt(i);
+
+			answer += (ch - 'a' + 1) * (Math.pow(base, i) % mod);
+		}
+		answer %= mod;
+		System.out.println(answer);
+	}
+}
