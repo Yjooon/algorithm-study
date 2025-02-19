92677d102b8c338d377fc989d1115ac60d829343
YeongJoon <60257302+Yjooon@users.noreply.github.com>
2025-02-19 17:40:49 +0900

    [Silver V] Title: 다리 놓기, Time: 84 ms, Memory: 12252 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static long[][] dp = new long[30][30];
+
+    private static void calculateCombinations() {
+        for (int i = 0; i < 30; i++) {
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+        }
+
+        for (int i = 1; i < 30; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        calculateCombinations();
+
+        for (int testCase = 1; testCase <= T; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            System.out.println(dp[M][N]);
+        }
+    }
+}
